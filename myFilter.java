a25296b16b1c8e115489155f5ce475315bc73423
Grant <6433584+gdeshazer@users.noreply.github.com>
2017-05-10 22:10:30 -0600

    Minor Changes

--- myFilter.java
+++
@@ -1,5 +0,0 @@
-(sex|horny|titt|erotic|kink|bdsm|dirty|nude|fuck|boob|butt|" +
-            "milf|fap|blowjob|blowie|handjob|handie|cleavage|hentai|suicide|xxx|tiddies|bbt|rack|busty|violence|" +
-            "nsfw|naughty|mature|pussy|collectorofnutsfan|deactivated|tinyurl|withbenefits|cams|beelzebub|porn|daddys|tied|" +
-            "slave|nazi|thick|thot|waifu|pervert|gif|dildo|choking|furry|landingstrips|self(ie|y)|teen|browni|" +
-            "kingformygueen|gkgd|drugs|meth|stripper|slaughter|hoe|teanager)
